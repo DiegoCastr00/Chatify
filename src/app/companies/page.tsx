d8dbcc3aa0c771892ce2749c49cdc4aaffdf3987
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-08-05 23:00:36 -0600

    new build

--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -24,7 +24,7 @@ export default function Companies() {
     }
 
     const createCompanyComponent = (company: Company) => {
-        return () => (
+        return (
             <div className='flex-col h-full ml-10 w-full'>
                 <div className='w-full items-center justify-center self-center'>
                     <p className="text-black dark:text-white text-xl">
@@ -57,45 +57,45 @@ export default function Companies() {
     const cards = companies.flatMap((company, index) => {
         const isEvenRow = Math.floor(index / 3) % 2 === 0;
         const positionInRow = index % 3;
-      
+
         let aspect, colSpan;
-      
+
         if (isEvenRow) {
-          if (positionInRow === 0) {
-            aspect = "16/10";
-            colSpan = "col-span-2";
-          } else {
-            aspect = "4/3";
-            colSpan = "col-span-1";
-          }
+            if (positionInRow === 0) {
+                aspect = "16/10";
+                colSpan = "col-span-2";
+            } else {
+                aspect = "4/3";
+                colSpan = "col-span-1";
+            }
         } else {
-          if (positionInRow === 2) {
-            aspect = "16/10";
-            colSpan = "col-span-2";
-          } else {
-            aspect = "4/3";
-            colSpan = "col-span-1";
-          }
+            if (positionInRow === 2) {
+                aspect = "16/10";
+                colSpan = "col-span-2";
+            } else {
+                aspect = "4/3";
+                colSpan = "col-span-1";
+            }
         }
-      
+
         return {
-          id: company.id,
-          content: React.createElement(Skeletons[company.id]),
-          className: `${colSpan} aspect-[${aspect}]`,
-          height: `${colSpan} h-56`,
-          thumbnail: `/assets/${company.img}`,
-          company: company,
+            id: company.id,
+            content: React.createElement(Skeletons[company.id]),
+            className: `${colSpan} aspect-[${aspect}]`,
+            height: `${colSpan} h-56`,
+            thumbnail: `/assets/${company.img}`,
+            company: company,
         };
-      });
+    });
 
     return (
         <motion.div
             initial={{ opacity: 0.0, y: 40 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
+                delay: 0.3,
+                duration: 0.8,
+                ease: "easeInOut",
             }}
             className="relative flex items-center justify-center px-4 w-full h-full"
         >
